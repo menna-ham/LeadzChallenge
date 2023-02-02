17fde5ef2261b387699adb0ead9ba28f09211453
menna-ham <menna.hamouda58@gmail.com>
2023-02-02 06:22:20 +0200

    Removing ApiKey from comments

--- src/Components/AllUsers/AllUsers.jsx
+++ src/Components/AllUsers/AllUsers.jsx
@@ -10,14 +10,7 @@ export const AllUsers = () => {
         let {data} = await axios.get('https://jsonplaceholder.typicode.com/users');
         console.log(data);
         setAllUsers(data);
-        //https://maps.googleapis.com/maps/api/geocode/json?latlng=-37.3159,81.1496&key=AIzaSyCDLWjbFMQwtv088sGrVOkZ5oy73NZhmuE
-        //https://www.openstreetmap.org/#map=18/-37.3159/81.1496`
-        //https://www.mapquestapi.com/geocoding/v1/reverse?key=Q23FGz00vMDynjbaAdLB5bXAYAMDTSKO&location=37.3159%2C81.1496&outFormat=json&thumbMaps=false'
-        //https://geocode.maps.co/reverse?lat=-37.3159&lon=81.1496
-                //AIzaSyCDLWjbFMQwtv088sGrVOkZ5oy73NZhmuE
-        //https://maps.googleapis.com/maps/api/js?key=AIzaSyCDLWjbFMQwtv088sGrVOkZ5oy73NZhmuE&libraries=places
-        //https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name%2Crating%2Cformatted_phone_number&key=AIzaSyCDLWjbFMQwtv088sGrVOkZ5oy73NZhmuE
-    }
+ }
 
 
     useEffect(()=>{
